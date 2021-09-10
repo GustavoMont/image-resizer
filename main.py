517285d9ebcdaf326a8f1b95ca06033ad566706c
GustavoMont <lgmluisgm@gmail.com>
2021-09-10 09:57:55 -0300

    make it keeping the aspect ratio

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 try:
     os.mkdir('results')
+    os.system('cls')
 except:
     os.system('cls')
 
@@ -11,22 +12,23 @@ def is_imagem(item):
         return True
     return False
 
-def reduzir_imagem(input_dir, output_dir):    
-    proporcao = int(input('Quer a imagem quantas vezes menor:\nDigite aqui o valor: '))
+def reduzir_imagem(input_dir, output_dir):
+    nova_width = int(input('Quanto de largura você deseja?\nDigite aqui o número: '))
     todos_arquivos = os.listdir(input_dir)
     for item in todos_arquivos:
         if is_imagem(item):
             imagem = Image.open(os.path.join(input_dir, item))
-            tamanho = (int(imagem.size[0]/proporcao), int(imagem.size[1]/proporcao))
-            nova_imagem = imagem.resize(tamanho)
-            nova_imagem.save(os.path.join(output_dir, item))
-    
+            widhtImage, heightImage = imagem.size[0], imagem.size[1]
+            nova_heigth = round(nova_width*heightImage/widhtImage)
+            tamanho = ((nova_width, nova_heigth))
+            imagem.thumbnail(tamanho)
+            imagem.save(os.path.join(output_dir, item), optimize=True, quality=100)
 
 while True:
     input_dir = input('Digite o caminho de onde estão as imagens: ')
     reduzir_imagem(input_dir, './results')
     opc = input('Deseja redimensionar mais imagens?[yes/no]\nDigite aqui: ')
-    if opc.lower() == 'nao' or opc.lower() == 'no':
+    if not (opc.lower() == 'yes' or opc.lower() == 'sim'):
         print('Okay! ;)')
-        break;
+        break
 
